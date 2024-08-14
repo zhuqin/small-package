a1e72ee4093fefd395b4d021ce261e2cc1e66257
zhuqin <zhuqin@users.noreply.github.com>
2024-08-14 16:11:47 +0800

    update 2024-08-14 16:11:47

--- luci-app-openvpn-server/luasrc/controller/openvpn-server.lua
+++ luci-app-openvpn-server/luasrc/controller/openvpn-server.lua
@@ -1,30 +1,33 @@
--- Copyright 2021-2022 Lienol <lawlienol@gmail.com>
-module("luci.controller.openvpn-server", package.seeall)
-
+module("luci.controller.openvpn-server",package.seeall)
 function index()
-    if not nixio.fs.access("/etc/config/luci-app-openvpn-server") then return end
-
-    entry({"admin", "vpn"}, firstchild(), "VPN", 45).dependent = false
-    entry({"admin", "vpn", "openvpn-server"}, alias("admin", "vpn", "openvpn-server", "settings"), _("OpenVPN Server"), 50).acl_depends = { "luci-app-openvpn-server" }
-    entry({"admin", "vpn", "openvpn-server", "settings"}, cbi("openvpn-server/settings"), _("Server"), 10).leaf = true
-    entry({"admin", "vpn", "openvpn-server", "user"}, cbi("openvpn-server/user")).leaf = true
-    entry({"admin", "vpn", "openvpn-server", "online"}, cbi("openvpn-server/online"), _("Online Users"), 11).leaf = true
-    entry({"admin", "vpn", "openvpn-server", "log"}, form("openvpn-server/log"), _("Logs"), 99).leaf = true
-    entry({"admin", "vpn", "openvpn-server", "status"}, call("status")).leaf = true
-    entry({"admin", "vpn", "openvpn-server", "get_log"}, call("get_log")).leaf = true
+	if not nixio.fs.access("/etc/config/openvpn") then
+		return
+	end
+	entry({"admin","vpn"},firstchild(),"VPN",45).dependent=false
+	local e=entry({"admin","vpn","openvpn-server"},firstchild(),_("OpenVPN Server"),1)
+	e.dependent=false
+	e.acl_depends={ "luci-app-openvpn-server" }
+	entry({"admin","vpn","openvpn-server","base"},cbi("openvpn-server/base"),_("Base Setting"),1)
+	entry({"admin","vpn","openvpn-server","client"},form("openvpn-server/client"),_("Client configuration"),2)
+	entry({"admin","vpn","openvpn-server","passwd"},form("openvpn-server/passwd"),_("Username and Password"),3)
+	entry({"admin","vpn","openvpn-server","vars"},form("openvpn-server/vars"),_("Certificate option"),4)
+	entry({"admin","vpn","openvpn-server","log"},template("openvpn-server/log"),_("Log"),5)
+	entry({"admin","vpn","openvpn-server","run"},call("act_status"))
+	entry({"admin","vpn","openvpn-server","logdata"},call("act_log"))
 end
 
-function status()
-    local e = {}
-    e.status = luci.sys.call("top -bn1 | grep -v grep | grep '/var/etc/openvpn-server' >/dev/null") == 0
-    luci.http.prepare_content("application/json")
-    luci.http.write_json(e)
+function act_status()
+	local e={}
+	e.running=luci.sys.call("pgrep openvpn >/dev/null")==0
+	e.cert=luci.sys.call("[ -s /etc/openvpn/ca.crt -a -s /etc/openvpn/client.crt -a -s /etc/openvpn/client.key -a -s /etc/openvpn/dh.pem -a -s /etc/openvpn/server.crt -a -s /etc/openvpn/server.key ] \\\
+&& [ -s /etc/openvpn/ta.key -o -z \"$(uci -q get openvpn.myvpn.tls_auth)\" ] || exit 1")==0
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
 end
 
-function get_log()
-    local e = {}
-    e.app_log = luci.sys.exec("[ -f '/tmp/etc/openvpn-server/openvpn.log' ] && cat /tmp/etc/openvpn-server/openvpn.log")
-    e.auth_log = luci.sys.exec("[ -f '/tmp/etc/openvpn-server/client.log' ] && cat /tmp/etc/openvpn-server/client.log")
-    luci.http.prepare_content("application/json")
-    luci.http.write_json(e)
+function act_log()
+	local log_data={}
+	log_data.syslog=luci.sys.exec("logread | grep 'openvpn(' | sed -e 's/daemon\\S* //' -e 's/([^ ]*\\]//' -e 's/[^ ]* //'")
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(log_data)
 end
